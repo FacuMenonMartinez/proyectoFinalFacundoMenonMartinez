c346ef146c36e71789ce214086d5042424b3d175
Facundo Menon Martinez <facundomenon.m@gmail.com>
2023-06-15 01:29:46 -0300

    Carrito con storage

--- js/main.js
+++ js/main.js
@@ -1,23 +1,70 @@
+let carrito = []
 
+// Generador de tarjetas
 let divOfertas=document.getElementById("novedades");
-
-// GENERAR FILTRO PARA QUE SE VEAN UNICAMENTE LOS QUE ESTAN EN OFERTA
+const productosOferta=[];
+// PRODUCTOS
+// Mostrar unicamente los de la oferta y funcion de botones tarjeta
+// 1-Los capturo del JSON
 fetch(`./js/productos.json`)
     .then((response) => response.json())
     .then((productos) => {
-        // For of para recorrer el arreglo y luego se le aplica el filtro
-        for (oferta of productos.filter(clasicos=>clasicos.genero==="Clasico")){
-            let tarjeta = document.createElement("div");
-            tarjeta.className ="tarjeta"
-            tarjeta.innerHTML = `
-                    <img class="imgLibro" src="${oferta.img}"
-                        alt="">
-                    <h4>${oferta.nombre}</h4>
-                    <b>$${oferta.precio}</b>
-                    <div class="botonesTarjeta">
-                        <button>Ver más</button>
-                        <button>Agregar al carrito</button>
-                    </div>`
-            divOfertas.append(tarjeta);
-        }    
+        productos.forEach(novedad=>{
+            // Filtro para que use solo los clasicos
+            // La opcion filter no funciona con los botones(marca siempre el mismo producto)
+            // Si el JSON no esta ordenado, no muestra todos los productos del filtro en este caso
+            if(novedad.genero==="Clasico"){
+                let tarjeta = document.createElement("div");
+                tarjeta.className ="tarjeta"
+                tarjeta.innerHTML = `
+                        <img class="imgLibro" src="${novedad.img}"
+                            alt="">
+                        <h4>${novedad.nombre}</h4>
+                        <b>$${novedad.precio}</b>
+                        <div class="botonesTarjeta">
+                            <button id="verMas${novedad.id}">Ver más</button>
+                            <button id="agregarCarrito${novedad.id}">Agregar al carrito</button>
+                        </div>`
+                divOfertas.append(tarjeta);
+
+        // // Botones de la tarjeta
+        let botonAgregarCarrito = document.getElementById(`agregarCarrito${novedad.id}`);
+        botonAgregarCarrito.addEventListener("click",()=>{agregarCarrito (productos, novedad.id)});
+            }else{
+            };
+
+
+
+        })
+        
     });
+
+// Funcion para evento del carrito
+const agregarCarrito = (productos, seleccionador) =>{
+    let libroSeleccionado = productos.find(item=>item.id===seleccionador);
+    carrito.push(libroSeleccionado);
+
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+
+    console.log("Producto agregado al carrito");
+};
+
+
+const mantenerCarrito=()=>{
+    let carritoStorage = JSON.parse(sessionStorage.getItem("carrito"));
+// Recorro el storage en caso que haya y vuelvo a pushear los productos que ya estan al carrito
+// para que no se sobreescriba en caso que siga agregando productos
+ if(carritoStorage){
+    carritoStorage.forEach(item=>carrito.push(item));
+ }
+
+}
+
+mantenerCarrito();
+
+
+
+
+
+
+
--- js/prueba.js
+++ js/prueba.js
@@ -1,35 +1,35 @@
-let divOfertas = document.getElementById("ofertas");
+// let divOfertas = document.getElementById("ofertas");
 
 
-let objetoPrueba = {
-    id: 1,
-    nombre: "La isla del tesoro",
-    autor: "Robert Stevenson",
-    genero: "Clásico",
-    precio: 2500,
-    img: "../recursos/libros/islaTesoro.jpg"
-};
-let objetoPrueba2 = {
-    id: 1,
-    nombre: "La isla del tesoro",
-    autor: "Robert Stevenson",
-    genero: "asdas",
-    precio: 2500,
-    img: "../recursos/libros/islaTesoro.jpg"
-};
-let objetoPrueba3 = {
-    id: 1,
-    nombre: "Misery",
-    autor: "Robert Stevenson",
-    genero: "Clásico",
-    precio: 2500,
-    img: "../recursos/libros/islaTesoro.jpg"
-};
+// let objetoPrueba = {
+//     id: 1,
+//     nombre: "La isla del tesoro",
+//     autor: "Robert Stevenson",
+//     genero: "Clásico",
+//     precio: 2500,
+//     img: "../recursos/libros/islaTesoro.jpg"
+// };
+// let objetoPrueba2 = {
+//     id: 1,
+//     nombre: "La isla del tesoro",
+//     autor: "Robert Stevenson",
+//     genero: "asdas",
+//     precio: 2500,
+//     img: "../recursos/libros/islaTesoro.jpg"
+// };
+// let objetoPrueba3 = {
+//     id: 1,
+//     nombre: "Misery",
+//     autor: "Robert Stevenson",
+//     genero: "Clásico",
+//     precio: 2500,
+//     img: "../recursos/libros/islaTesoro.jpg"
+// };
 
-const arreglo =[];
-arreglo.push(objetoPrueba);
-arreglo.push(objetoPrueba2);
-arreglo.push(objetoPrueba3);
+// const arreglo =[];
+// arreglo.push(objetoPrueba);
+// arreglo.push(objetoPrueba2);
+// arreglo.push(objetoPrueba3);
 
 // console.log(arreglo);
 
@@ -62,26 +62,44 @@ arreglo.push(objetoPrueba3);
 
 // console.log(objetoPrueba);
 
-
 // GENERAR FILTRO PARA QUE SE VEAN UNICAMENTE LOS QUE ESTAN EN OFERTA
-fetch(`./productos.json`)
-    .then((response) => response.json())
-    .then((productos) => {
-        // For of para recorrer el arreglo y luego se le aplica el filtro
-        for (oferta of productos.filter(clasicos=>clasicos.genero==="Clasico")){
-            let tarjeta = document.createElement("div");
-            tarjeta.className ="tarjeta"
-            tarjeta.innerHTML = `
-                    <img class="imgLibro" src="${oferta.img}"
-                        alt="">
-                    <h4>${oferta.nombre}</h4>
-                    <b>$${oferta.precio}</b>
-                    <div class="botonesTarjeta">
-                        <button>Ver más</button>
-                        <button>Agregar al carrito</button>
-                    </div>`
-            divOfertas.append(tarjeta);
-        }    
-    });
+// fetch(`./productos.json`)
+//     .then((response) => response.json())
+//     .then((productos) => {
+//         // For of para recorrer el arreglo y luego se le aplica el filtro
+//         for (oferta of productos.filter(clasicos=>clasicos.genero==="Clasico")){
+//             pruebaArray.push(oferta);
+//         }    
+//     });
 
+fetch (`./productos.json`)
+.then((response)=>response.json())
+.then((data)=>{
+    for(item of data.filter(clasicos => clasicos.genero==="Clasico")){
 
+        libros.push(item)
+    }
+})
+let libros=[];
+
+console.log(libros);
+
+let divOfertas=document.getElementById("ofertas");
+
+let saludo=document.createElement("h1");
+saludo.innerHTML=`HOLA`;
+divOfertas.append(saludo)
+
+libros.forEach(oferta=>{
+    let tarjeta1=document.createElement("div");
+    tarjeta1.className ="tarjeta";
+    tarjeta1.innerHTML=`
+    <h4>${oferta.nombre}</h4>
+    <div class="botonesTarjeta">
+    <button id="verMas${oferta.id}">Ver mas</button>
+    <button id="agregarCarrito${oferta.id}">Agregar Carrito</button>
+    </div>
+    `
+    divOfertas.append(tarjeta1);
+    console.log(oferta[1].nombre)
+})
